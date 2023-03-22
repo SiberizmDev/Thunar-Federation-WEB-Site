46f7f9f9b224929e6d22eb71dcb0115740a4e1c1
AhmetinzmSec <picadroattack@outlook.com>
2023-03-22 11:54:07 +0300

    sa

--- src/config.mjs
+++ src/config.mjs
@@ -1,13 +1,13 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'Thunar Federasyonu',
+  name: 'Kapgan Federasyonu',
 
-  origin: 'https://thunarfederation.vercel.app/',
+  origin: 'https://Kapganfederation.vercel.app/',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'Thunar Federasyonu ・ Siber Güvenlik ve Modern Tasarımın Öncüleri',
+  title: 'Kapgan Federasyonu ・ Siber Güvenlik ve Modern Tasarımın Öncüleri',
   description:
     'Hem güvenlik, hemde yeni nesil teknolojileri içerisinde bulunduran şirketimiz ile arkadaşlarınıza hava atın',
   defaultImage: defaultImage,
